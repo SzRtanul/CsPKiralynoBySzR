cef67372ad56eb906241659f6441854339bffdcc
Szabó Roland <szrtanul@gmail.com>
2024-12-09 09:33:36 +0100

    f1 3

--- src/main/java/hu/szamalk/model/Tabla.java
+++ src/main/java/hu/szamalk/model/Tabla.java
@@ -10,5 +10,30 @@ package hu.szamalk.model;
  */
 public class Tabla {
     private char[][] T;
+    private char uresCella;
+    
+    public Tabla(char uresCella){
+        T = new char[8][8];
+        this.uresCella = uresCella;
+        
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                T[i][j] = this.uresCella;
+            }
+        }
+    }
+    
+    public int getUresOszlopokSzama(){
+        return 0;
+    }
+    
+    public int getUresSorokSzama(){
+        return 0;
+    }
+    
+    public boolean doElhelyez(){
+        return false;
+    }
+    
     
 }
